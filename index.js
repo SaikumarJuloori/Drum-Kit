b0eb17a9423641fc28a53a0b7c3ad1e6849a8f87
SaikumarJuloori <94303636+SaikumarJuloori@users.noreply.github.com>
2024-09-25 17:44:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ document.addEventListener("keypress", function(event){
 function makeSound(key){
 switch (key) {
   case "w":
-  var crash = new Audio('sounds/crash.mp3');
+  var crash = new Audio('crash.mp3');
   crash.play();
     break;
     case "a":
-      var kick = new Audio('sounds/kick-bass.mp3');
+      var kick = new Audio('kick-bass.mp3');
       kick.play();
       break;
     case "s":
@@ -31,19 +31,19 @@ switch (key) {
       snare.play();
       break;
       case "d":
-        var tom1 = new Audio('sounds/tom-1.mp3');
+        var tom1 = new Audio('tom-1.mp3');
         tom1.play();
         break;
         case"j":
-          var tom2 = new Audio('sounds/tom-2.mp3');
+          var tom2 = new Audio('tom-2.mp3');
           tom2.play();
           break;
           case "k":
-            var tom3 = new Audio('sounds/tom-3.mp3');
+            var tom3 = new Audio(tom-3.mp3');
             tom3.play();
             break;
             case "l":
-              var tom4 = new Audio('sounds/tom-4.mp3');
+              var tom4 = new Audio(tom-4.mp3');
               tom4.play();
               break;
   default: console.log(key);
